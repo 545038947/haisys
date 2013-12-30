36a8399d2b92d5abca11f21c1fcae75e0aba6556
545038947 <545038947@qq.com>
2013-12-30 17:13:10 +0800

    Signed-off-by: 545038947 <545038947@qq.com>

---
+++ Public/autocode/modeltmpl/Model.class.php
@@ -0,0 +1,23 @@
+<?php
+// +----------------------------------------------------------------------
+// | Author: 和蔼的木Q <545038947@qq.com>
+// +----------------------------------------------------------------------
+
+namespace Admin\Model;
+use Think\Model;
+
+/**
+ * 代码生成配置模型
+ * @author 和蔼的木Q <545038947@qq.com>
+ */
+
+class AutocodeModel extends Model {
+    protected $_validate = array(
+        [#validate]
+    );
+
+    protected $_auto = array(
+        
+    );
+
+}
