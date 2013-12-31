f9c28d1d9e1ac4e4e6b0a715a208628cb69a5fd8
545038947 <545038947@qq.com>
2013-12-31 18:38:21 +0800

    Signed-off-by: 545038947 <545038947@qq.com>

---
+++ Application/Admin/Model/AutocadetestModel.class.php
@@ -0,0 +1,23 @@
+<?php
+// +----------------------------------------------------------------------
+// | Author: 和蔼的木Q <545038947@qq.com>
+// +----------------------------------------------------------------------
+
+namespace Admin\Model;
+use Think\Model;
+
+/**
+ * ______模型
+ * @author 和蔼的木Q <545038947@qq.com>
+ */
+
+class AutocadetestModel extends Model {
+    protected $_validate = array(
+        
+    );
+
+    protected $_auto = array(
+        
+    );
+
+}
